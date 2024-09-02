79c4f99d360abc20912cbab5f1e6e183992edcdc
water <water.wei@me.com>
2024-09-02 11:51:04 +0800

    test action again

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 设置程序名称
+# 设置程序名称 / Set the application name
 APP_NAME="ollama_bench"
 
-# 创建一个输出目录
+# 创建一个输出目录 / Create an output directory
 mkdir -p build
 
-# 定义目标平台
+# 定义目标平台 / Define target platforms
 platforms=(
     "windows/amd64"
     "windows/386"
@@ -18,30 +18,30 @@ platforms=(
     "linux/arm64"
 )
 
-# 遍历并编译每个平台
+# 遍历并编译每个平台 / Iterate and compile for each platform
 for platform in "${platforms[@]}"
 do
-    # 分割平台字符串
+    # 分割平台字符串 / Split the platform string
     IFS='/' read -r -a array <<< "$platform"
     GOOS=${array[0]}
     GOARCH=${array[1]}
     
-    # 设置输出文件名
+    # 设置输出文件名 / Set the output file name
     if [ $GOOS = "windows" ]; then
         output_name=$APP_NAME'_'$GOOS'_'$GOARCH'.exe'
     else
         output_name=$APP_NAME'_'$GOOS'_'$GOARCH
     fi
 
-    # 编译
-    echo "编译 $output_name"
+    # 编译 / Compile
+    echo "Compiling $output_name"
     env GOOS=$GOOS GOARCH=$GOARCH go build -o build/$output_name main.go
     
     if [ $? -ne 0 ]; then
-        echo "编译 $output_name 失败"
+        echo "Failed to compile $output_name"
     else
-        echo "编译 $output_name 成功"
+        echo "Successfully compiled $output_name"
     fi
 done
 
-echo "编译完成"
+echo "Compilation completed"
